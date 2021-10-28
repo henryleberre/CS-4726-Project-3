28f40ef4cf1d366b1322ae06f4572e0667825b32
Henry Le Berre <24208000+henryleberre@users.noreply.github.com>
2021-10-28 16:03:16 -0400

    Commit #4

--- App.js
+++ App.js
@@ -11,29 +11,38 @@ import {
   MenuOption as PopupMenuOption,
   MenuTrigger as PopupMenuTrigger,
 } from 'react-native-popup-menu';
+import QRCode from 'react-native-qrcode-svg';
 
 const Stack = createNativeStackNavigator();
 
-function Dashboard_EventCard({ style, eventData }) {
+const EVENT_DATA = [
+  {image: require('./assets/moon.jpg'),       name: "Moon Landing Skeptics",      date: "11/17/2021", nPeople: 420,  location: "Atlanta, GA"},
+  {image: require('./assets/ussr.png'),       name: "Our Communist Party",        date: "11/23/2021", nPeople: 627,  location: "Atlanta, GA"},
+  {image: require('./assets/illuminati.jpg'), name: "New World Order Resistance", date: "11/23/2021", nPeople: 1298, location: "Atlanta, GA"},
+];
+
+const PageOuterPaddingView = ({children, style}) => <View style={Object.assign({}, style, {marginHorizontal: 12})}>{children}</View>;
+
+function Dashboard_StatusCard({event, navigation, style}) {
   return (
-    <TouchableOpacity style={style}>
+    <TouchableOpacity onPressOut={() => {navigation.navigate("Event");}} style={style}>
       <Card borderRadius={10} enableShadow={true}>
         <View style={{display: "flex", alignItems: "center", flexDirection: "row", height: 100 }}>
-          <Image style={{height: 100, width: 100}} borderTopLeftRadius={10} source={eventData.image} resizeMode="cover" />
+          <Image style={{height: 100, width: 100}} borderTopLeftRadius={10} source={event.image} resizeMode="cover" />
           <View style={{flex: 1, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-            <Text style={{paddingVertical: 5}} text60BL center={true}>{eventData.name}</Text>
-            <Text style={{paddingVertical: 5}} text70 center={true}>{eventData.description}</Text>              
+            <Text style={{paddingVertical: 5}} text60BL center={true}>{event.name}</Text>
+            <Text style={{paddingVertical: 5}} text70 center={true}>{event.date}</Text>              
           </View>
         </View>
         <View flex paddingVertical={10} style={{paddingHorizontal: 10, flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
-          <View flex style={{flex:1, flexDirection: "row", alignItems: "center", justifyContent: "space-evenly"}}>
-            <View flex style={{flexDirection: "row", alignItems: "center"}}>
+          <View flex style={{flex:1, flexDirection: "row", justifyContent: "space-around"}}>
+            <View flex style={{flex:0, flexDirection: "row", alignItems: "center"}}>
               <Ionicons name="people" size={24} color="black" />
-              <Text text70> {eventData.nPeople}</Text>
+              <Text text70> {event.nPeople}</Text>
             </View>
-            <View flex style={{flexDirection: "row", alignItems: "center"}}>
+            <View flex style={{flex:0, flexDirection: "row", alignItems: "center"}}>
               <Ionicons name="location" size={24} color="black" />
-              <Text text70>{eventData.location}</Text>
+              <Text text70> {event.location}</Text>
             </View>
           </View>
           <Ionicons name="arrow-forward-sharp" size={30} color="black" />
@@ -43,19 +52,50 @@ function Dashboard_EventCard({ style, eventData }) {
   );
 }
 
-function Dashboard_StatusList() {
-  const eventData = [
-    {image: require('./assets/moon.jpg'),       name: "Moon Landing Skeptics",      description: "11/17/2021", nPeople: 420, location: "Atlanta, GA"},
-    {image: require('./assets/ussr.png'),       name: "Our Communist Party",        description: "11/23/2021", nPeople: 627,  location: "Atlanta, GA"},
-    {image: require('./assets/illuminati.jpg'), name: "New World Order Resistance", description: "11/23/2021", nPeople: 1298,  location: "Atlanta, GA"},
-  ];
+function App_Event({ navigation }) {
+  const event = EVENT_DATA[0];
 
+  return (
+    <View flex>
+      <Text style={{paddingVertical: 5}} center text50>{event.name}</Text>
+      <Image style={{height: 150, width: "100%"}} source={event.image} resizeMode="cover" />
+      <PageOuterPaddingView flex style={{justifyContent: "center", alignItems: "center"}}>
+        <View flex style={{width: "100%", flexDirection: "row", justifyContent: "space-around", marginVertical: 20}}>
+          <View flex style={{flex:0, flexDirection: "row", alignItems: "center"}}>
+            <Ionicons name="people" size={24} color="black" />
+            <Text text70> {event.nPeople}</Text>
+          </View>
+          <View flex style={{flex:0, flexDirection: "row", alignItems: "center"}}>
+            <Ionicons name="location" size={24} color="black" />
+            <Text text70> {event.location}</Text>
+          </View>
+          <View flex style={{flex:0, flexDirection: "row", alignItems: "center"}}>
+            <Ionicons name="calendar" size={24} color="black" />
+            <Text text70> {event.date}</Text>
+          </View>
+        </View>
+        <View flex backgroundColor="#000000" style={{justifyContent: "center", alignItems: "center", borderRadius: 20, padding: 20, marginVertical: 20}}>
+          <QRCode
+            enableLinearGradient
+            backgroundColor="#000000"
+            size={200}
+            linearGradient={['rgb(255,255,255)','rgb(255,0,255)']	}
+            value="https://youtu.be/dQw4w9WgXcQ"
+          />
+        </View>
+        <Text center style={{marginVertical: 20}}>
+          QR-Codes regenerate every 10s and disappear once used
+        </Text>
+      </PageOuterPaddingView>
+    </View>
+  );
+}
+
+function Dashboard_StatusList(navigation) {
   return (
     <ScrollView>
     {
-      eventData.map((e, i) => {
-        return <Dashboard_EventCard key={i} style={{ paddingTop: i != 0 ? 20 : 0 }} eventData={e} />
-      })
+      EVENT_DATA.map((e, i) => <Dashboard_StatusCard key={i} navigation={navigation} event={e} style={{ paddingTop: i != 0 ? 20 : 0 }} />)
     }
     </ScrollView>
   );
@@ -65,14 +105,14 @@ function Dashboard_Status() {
   return (
     <Card borderRadius={10} style={{display: "flex", flexDirection: "row", paddingHorizontal: 12, padding: 15}}>
       <View flex style={{flex:1, justifyContent: "center", alignItems: "center"}}>
-        <Text center text60>Vaccine</Text>
-        <AntDesign style={{marginVertical: 10}} name="checkcircle" size={40} color="green" />
-        <Text text70>2/2</Text>
+        <Text center text60>Vaccine(s)</Text>
+        <FontAwesome style={{marginVertical: 10}} name="check-circle" size={40} color="green" />
+        <Text text70>2/2 & 2 weeks</Text>
       </View>
       <View flex style={{flex:1, justifyContent: "center", alignItems: "center"}}>
-        <Text center text60>Tests</Text>
+        <Text center text60>Test(s)</Text>
         <FontAwesome style={{marginVertical: 10}} name="warning" size={40} color="orange" />
-        <Text text70>Test has expired</Text>
+        <Text text70>Expired</Text>
       </View>
     </Card>
   );
@@ -142,7 +182,7 @@ function Dashboard_OpenSource() {
 }
 
 // sections = { name, obj_right, func }
-function GenerateSections(sections) {
+function GenerateSections(sections, navigation) {
   return (
     <>
       {
@@ -153,7 +193,7 @@ function GenerateSections(sections) {
                 <Text text50 heading>{e.name}</Text>
                 {e.obj_right}
               </View>
-              {e.func()}
+              {e.func(navigation)}
             </View>
           );
         })
@@ -184,7 +224,7 @@ function IconPopupMenu({ touchable, items }) {
   );
 }
 
-function App_Dashboard() {
+function App_Dashboard(navigation) {
   const statusPopupItems = [
     {
       text: "Add Vaccine",
@@ -211,65 +251,84 @@ function App_Dashboard() {
     }
   ];
 
-  return GenerateSections([
-    { 
-      name: "Your Status",
-      obj_right: (<IconPopupMenu items={statusPopupItems} touchable={<Ionicons name="add-circle-outline" size={40} color="black" />}></IconPopupMenu>), 
-      func: Dashboard_Status
-    },
-    { name: "Your Upcoming Events",
-      obj_right: (<IconPopupMenu items={eventsPopupItems} touchable={<Ionicons name="add-circle-outline" size={40} color="black" />}></IconPopupMenu>),
-      func: Dashboard_StatusList
-    }
-  ]);
+  return (
+    <PageOuterPaddingView>
+      {GenerateSections([
+        { 
+          name: "Your Status",
+          obj_right: (<IconPopupMenu items={statusPopupItems} touchable={<Ionicons name="add-circle-outline" size={40} color="black" />}></IconPopupMenu>), 
+          func: Dashboard_Status
+        },
+        { name: "Your Upcoming Events",
+          obj_right: (<IconPopupMenu items={eventsPopupItems} touchable={<Ionicons name="add-circle-outline" size={40} color="black" />}></IconPopupMenu>),
+          func: Dashboard_StatusList
+        }
+      ], navigation)}
+    </PageOuterPaddingView>
+  );
 }
 
-function App_Privacy() {
-  return GenerateSections([{
-    name: "Your Privacy",
-    obj_right: <></>,
-    func: Dashboard_Privacy
-  }]);
+function App_Privacy(navigation) {
+  return (
+    <PageOuterPaddingView>
+      {GenerateSections([{
+        name: "Your Privacy",
+        obj_right: <></>,
+        func: Dashboard_Privacy
+      }], navigation)}
+    </PageOuterPaddingView>
+  );
 }
 
-function App_OpenSource() {
-  return GenerateSections([{
-    name: "Open Source",
-    obj_right: <></>,
-    func: Dashboard_OpenSource
-  }]);
+function App_OpenSource(navigation) {
+  return (
+    <PageOuterPaddingView>
+    {GenerateSections([{
+      name: "Open Source",
+      obj_right: <></>,
+      func: Dashboard_OpenSource
+    }], navigation)}
+    </PageOuterPaddingView>
+  );
 }
 
 function ScreenHolder({navigation, func}) {
   return (
     <SafeAreaView style={{ flex:1, backgroundColor: "#FFFFFF" }}>
-      <ScrollView style={{backgroundColor: "white"}}>
-        <View flex padding-page style={{ marginHorizontal: 12, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}>
-          {func()}
-        </View>
-        <Text>{"\n"}{"\n"}</Text>
-      </ScrollView>
-      <App_Navbar navigation={navigation} />
+      <View flex padding-page style={{paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0}}>
+        <ScrollView style={{backgroundColor: "white"}}>
+          {func(navigation)}
+          <Text>{"\n"}{"\n"}</Text>
+        </ScrollView>
+        <App_Navbar navigation={navigation} />
+      </View>
     </SafeAreaView>
   );
 }
 
 const APP_SCREENS = [
-  { name: "Privacy",    icon_func: (color) => <Entypo    name="lock"   size={20} color={color} />, func: App_Privacy    },
-  { name: "Dashboard",  icon_func: (color) => <Entypo    name="home"   size={20} color={color} />, func: App_Dashboard  },
-  { name: "OpenSource", icon_func: (color) => <AntDesign name="github" size={20} color={color} />, func: App_OpenSource }
+  { bNavbarShow: true,  name: "Privacy",    icon_func: (color) => <Entypo    name="lock"   size={20} color={color} />, func: App_Privacy    },
+  { bNavbarShow: true,  name: "Dashboard",  icon_func: (color) => <Entypo    name="home"   size={20} color={color} />, func: App_Dashboard  },
+  { bNavbarShow: true,  name: "OpenSource", icon_func: (color) => <AntDesign name="github" size={20} color={color} />, func: App_OpenSource },
+  { bNavbarShow: false, name: "Event",      icon_func: () => {},                                                       func: App_Event      }
 ];
 
 function App_Navbar({ navigation, activeItemKey }) {
   const routes       = useNavigationState(state => state.routes);
   const currentRoute = routes[routes.length - 1].name;
 
-  const actions = APP_SCREENS.map((e, i) => {
-    return {
-      label: e.icon_func(currentRoute == e.name ? "#FF0000" : "#FFFFFF" ),
-      onPressOut: () => {navigation.navigate(e.name)}
-    };
-  });
+  const actions = Array.prototype.concat(
+    [{
+      label: <Ionicons name="ios-chevron-back-circle-sharp" size={20} color="white" />,
+      onPressOut: () => {navigation.goBack()}
+    }],
+    APP_SCREENS.filter((e) => e.bNavbarShow).map((e, i) => {
+      return {
+        label: e.icon_func(currentRoute == e.name ? "#FF0000" : "#FFFFFF" ),
+        onPressOut: () => {navigation.navigate(e.name)}
+      };
+    })
+  );
 
   return (
     <ActionBar centered backgroundColor="#000000" actions={actions} useSafeArea={true}></ActionBar>
